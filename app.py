26e4b9ce9d16cfaded6b5ddc1c78bed7acd622ad
Joshua Allum <joshuatallum@gmail.com>
2020-03-24 22:53:21 -0400

    Add server variable

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def getOptions(query=""):
 # App setup
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(children=[
     dcc.Markdown(d("""
