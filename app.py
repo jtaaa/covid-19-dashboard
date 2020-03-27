c8c447946ff1d09bcca829d5e88926b1b540dee1
Joshua Allum <joshuatallum@gmail.com>
2020-03-26 20:27:52 -0400

    Remove unnecessary comment

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import os
-import re
-# import argparse
 import pandas as pd
 import numpy as np
+import os
+import re
+
 from textwrap import dedent as d
 from dash.dependencies import Input, Output
 from data.manager import confirmed, dates
